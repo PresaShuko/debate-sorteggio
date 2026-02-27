a214cc95a25bcf6c6b65ec38d91066ad5bd63dd7
PresaShuko <pizzapalele345@gmail.com>
2026-02-27 23:46:21 +0100

    sistemato

--- debate-sorteggio/vite.config.js
+++ debate-sorteggio/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+
+  base: '/debate-sorteggio/',
+
   plugins: [
     vue(),
     VitePWA({
